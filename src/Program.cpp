c1c0d9a21b09beeede9bc66e2766b278032890a9
ZiKai <jinzikai@sjtu.edu.cn>
2025-11-27 16:54:46 +0800

    feat: complete Program & Recorder

--- src/Program.cpp
+++ src/Program.cpp
@@ -18,5 +18,63 @@ void Program::removeStmt(int line) {
 }
 
 void Program::run() {
-    
+    resetAfterRun();
+    if (recorder_.lines_.empty()) {
+        throw("SYNTAX ERROR");
+        return;
+    }
+    programCounter_ = recorder_.lines.begin()->first;
+    while (!programEnd_) {
+        Statement* curStmt = recorder_.get(programCounter_);
+        try {
+            this->execute(curStmt);
+        }
+        int nextLine = recorder_.nextLine(programCounter_);
+        if (nextLine == -1) {
+            programEnd_ = true;
+        }
+        else {
+            programCounter_ = nextLine;
+        }
+    }
+}
+
+void Program::list() const {
+    recorder_.printLines();
+}
+
+void Program::clear() {
+    recorder_.clear();
+    vars_.clear();
+
+}
+
+void Program::execute(Statement* stmt) {
+    // !stmt possible?
+    try {
+        stmt->execute(vars_, *this);
+    } catch (const BasicError& e) {
+        std::cout << e.message() << std::endl;
+    }
+}
+
+int Program::getPC() const noexcept {
+    return programCounter_;
+}
+
+void Program::changePC(int line) {
+    if (!recorder_.hasline(line)) {
+        throw BasicError("LINE NUMBER ERROR");
+    }
+    programCounter_ = line;
+}
+
+void Program::programEnd() {
+    programEnd_ = true;
+}
+
+void Program::resetAfterRun() noexcept {
+    programCounter_ = 0;
+    programEnd_ = false;
+    vars_.clear();
 }
--- src/Recorder.cpp
+++ src/Recorder.cpp
@@ -1,1 +1,30 @@
 // TODO: Imply interfaces declared in the Recorder.hpp.
+void Recorder::add(int line, Statement* stmt) {
+    lines_[line] = stmt;
+}
+void Recorder::remove(int line) {
+    lines_.erase(line);
+}
+const Statement* Recorder::get(int line) const noexcept {
+    return lines_[line];
+}
+bool Recorder::hasLine(int line) const noexcept {
+    if (lines_.find(line) == lines_.end()) {
+        return false;
+    }
+    else return true;
+}
+void Recorder::clear() noexcept {
+    lines_.clear();
+}
+void Recorder::printLines() const {
+    for (auto it = lines_.begin(); it != lines_.end(); ++it) {
+        std::cout << it->second->text() << std::endl;
+    }
+}
+int Recorder::nextLine(int line) const noexcept {
+    auto it = lines_.find(line);
+    ++it;
+    if (it == lines_.end()) return -1;
+    else return it->first;
+}
